dc3b0e8db54fb70a769204b9fabdfcd1e1e06aeb
Teamihailovska <mihailovska.a25167@next.edu.mk>
2026-05-12 19:22:37 +0200

    teaa

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('Homepage');
-});
+// Route::get('/', function () {
+//     return view('Homepage');
+// });
 
 Route::get('/AboutUs', function () {
     
