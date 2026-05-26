9ac215bbfe83ccedc81dbf66addbba1e36ee7246
monikakristo <kristo.a25168@next.edu.mk>
2026-05-26 19:28:08 +0200

    visitor

--- app/Http/Controllers/Admin/VisitRequestController.php
+++ app/Http/Controllers/Admin/VisitRequestController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 
 class VisitRequestController extends Controller
 {
-    private const MAX_DAILY_VISITS = 15;
+    private const MAX_DAILY_VISITS = 2;
     private const MAX_MONTHLY_VISITS = 2;
     private const CAPACITY_STATUSES = ['pending', 'approved'];
 
